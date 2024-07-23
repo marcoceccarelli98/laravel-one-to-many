06285d0429ef3d4848af45cb9de6693b88c2f340
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-23 15:48:50 +0200

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'start_date',
+        'end_date',
+        'images',
+        'slug',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+{{-- @dd($data['projects']) --}}
+
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        @if (auth()->check())
+            <div class="add-project">
+                <a class="btn btn-primary" href="{{ route('projects.create') }}">ADD NEW PROJECT</a>
+            </div>
+        @endif
+        <div class="container">
+            <div class="projects-list">
+                @foreach ($data['projects'] as $project)
+                    <div class="project-card">
+                        <a href="{{ route('projects.show', $project->slug) }}">
+                            <div class="image-container">
+                                <img src="{{ $project['images'][0] }}" alt="Thumb not found">
+                            </div>
+                            <div class="title-container">
+                                <p>{{ $project['title'] }}</p>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+@endsection
